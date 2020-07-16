1577413e4dbe49e77aecf0d9895ecf64fee0643f
ClaireTang <867641569@qq.com>
2020-07-16 08:30:31 +0800

    添加更新入口

--- App.vue
+++ App.vue
@@ -1,7 +1,43 @@
 <script>
+import { apiBaseUrl } from '@/config/config.js'
 export default {
 	onLaunch: function() {
 		console.log('App Launch');
+		// #ifdef APP-PLUS
+		var server = `${apiBaseUrl}merapi/Upgrade/getAllVersion`; //检查更新地址  
+		var req = { //升级检测数据  
+			"type": 'merchant',
+			"appid": plus.runtime.appid,
+			"version": plus.runtime.version
+		};
+		console.log(req)
+		// uni.showToast({
+		// 	title: req,
+		// 	duration: 4000
+		// })
+		uni.request({
+			url: server,
+			data: req,
+			success: (res) => {
+				// uni.showToast({
+				// 	title: res,
+				// 	duration: 4000
+				// })
+				console.log(res)
+				if (res.statusCode == 200 && res.data.status === 1) {
+					uni.showModal({ //提醒用户更新  
+						title: "更新提示",
+						content: res.data.note,
+						success: (res2) => {
+							if (res2.confirm) {
+								plus.runtime.openURL(res.data.url);
+							}
+						}
+					})
+				}
+			}
+		})
+		// #endif 
 		//获取省信息
 		this.$api.getAreaList({id:0}, res => {
 			if (res.state) {
